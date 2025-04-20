6d12e972b830fc07739ba590d1b63279474d51c2
atsumarukun <atys.smz@gmail.com>
2025-04-20 22:58:53 +0900

    fix: マイグレーションスクリプトの環境変数名を修正.

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -3,5 +3,5 @@
 if [ $# != 1 ] || [ $1 != "up" ] && [ $1 != "down" ]; then
   echo 不正な引数です
 else
-  migrate -path db/migrations -database "mysql://$MYSQL_USER:$MYSQL_PASSWORD@tcp($MYSQL_HOST:$MYSQL_PORT)/$MYSQL_DATABASE" $1
+  migrate -path db/migrations -database "mysql://$DATABASE_USER:$DATABASE_PASSWORD@tcp($DATABASE_HOST:$DATABASE_PORT)/$DATABASE_NAME" $1
 fi
