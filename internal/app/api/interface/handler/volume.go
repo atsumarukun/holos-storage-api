ddf65161dd22dc546eee2f0260f2215556d6310e
atsumarukun <atys.smz@gmail.com>
2025-04-23 01:27:59 +0900

    create: ボリュームハンドラを実装.

--- internal/app/api/interface/handler/volume.go
+++ internal/app/api/interface/handler/volume.go
@@ -1,8 +1,17 @@
 package handler
 
 import (
+	"net/http"
+
+	"github.com/atsumarukun/holos-storage-api/internal/app/api/interface/builder"
+	"github.com/atsumarukun/holos-storage-api/internal/app/api/interface/pkg/errors"
+	"github.com/atsumarukun/holos-storage-api/internal/app/api/interface/pkg/parameter"
+	"github.com/atsumarukun/holos-storage-api/internal/app/api/interface/schema"
+	"github.com/atsumarukun/holos-storage-api/internal/app/api/pkg/status"
+	"github.com/atsumarukun/holos-storage-api/internal/app/api/pkg/status/code"
 	"github.com/atsumarukun/holos-storage-api/internal/app/api/usecase"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type VolumeHandler interface {
@@ -19,4 +28,26 @@ func NewVolumeHandler(volumeUC usecase.VolumeUsecase) VolumeHandler {
 	}
 }
 
-func (h *volumeHandler) Create(c *gin.Context) {}
+func (h *volumeHandler) Create(c *gin.Context) {
+	var req schema.CreateVolumeRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		errors.Handle(c, status.Error(code.BadRequest, "bad request"))
+		return
+	}
+
+	accountID, err := parameter.GetContextParameter[uuid.UUID](c, "accountID")
+	if err != nil {
+		errors.Handle(c, err)
+		return
+	}
+
+	ctx := c.Request.Context()
+
+	volume, err := h.volumeUC.Create(ctx, accountID, req.Name, req.IsPublic)
+	if err != nil {
+		errors.Handle(c, err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, builder.ToVolumeResponse(volume))
+}
