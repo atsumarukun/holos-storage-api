c8da5d3aaeca7504b071154b4d78b1035fe63869
atsumarukun <atys.smz@gmail.com>
2025-04-23 22:19:27 +0900

    create: ボリュームリポジトリにUpdate関数とFindOneByIDAndAccountID関数を作成

--- internal/app/api/infrastructure/database/volume.go
+++ internal/app/api/infrastructure/database/volume.go
@@ -40,6 +40,14 @@ func (r *volumeRepository) Create(ctx context.Context, volume *entity.Volume) er
 	return err
 }
 
+func (r *volumeRepository) Update(ctx context.Context, volume *entity.Volume) error {
+	return errors.New("not implemented")
+}
+
+func (r *volumeRepository) FindOneByIDAndAccountID(ctx context.Context, id uuid.UUID, accountID uuid.UUID) (*entity.Volume, error) {
+	return nil, errors.New("not implemented")
+}
+
 func (r *volumeRepository) FindOneByNameAndAccountID(ctx context.Context, name string, accountID uuid.UUID) (*entity.Volume, error) {
 	driver := transaction.GetDriver(ctx, r.db)
 	var model model.VolumeModel
