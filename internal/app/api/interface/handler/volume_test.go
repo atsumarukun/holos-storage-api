39a9fa38f25c21f4046bb5229691462719471a8b
atsumarukun <atys.smz@gmail.com>
2025-04-24 22:14:18 +0900

    create: ボリュームリポジトリのGetOne関数にユニットテストコードを作成.

--- internal/app/api/interface/handler/volume_test.go
+++ internal/app/api/interface/handler/volume_test.go
@@ -356,3 +356,114 @@ func TestVolume_Delete(t *testing.T) {
 		})
 	}
 }
+
+func TestVolume_GetOne(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	id := uuid.New()
+	accountID := uuid.New()
+	volumeDTO := &dto.VolumeDTO{
+		ID:        id,
+		AccountID: uuid.New(),
+		Name:      "name",
+		IsPublic:  false,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	tests := []struct {
+		name            string
+		isSetID         bool
+		isSetAccountID  bool
+		expectCode      int
+		expectResponse  map[string]any
+		setMockVolumeUC func(context.Context, *mockUsecase.MockVolumeUsecase)
+	}{
+		{
+			name:           "success",
+			isSetID:        true,
+			isSetAccountID: true,
+			expectCode:     http.StatusOK,
+			expectResponse: map[string]any{"id": volumeDTO.ID.String(), "account_id": volumeDTO.AccountID.String(), "name": volumeDTO.Name, "is_public": volumeDTO.IsPublic, "created_at": volumeDTO.CreatedAt.Format(time.RFC3339Nano), "updated_at": volumeDTO.UpdatedAt.Format(time.RFC3339Nano)},
+			setMockVolumeUC: func(ctx context.Context, volumeUC *mockUsecase.MockVolumeUsecase) {
+				volumeUC.
+					EXPECT().
+					GetOne(ctx, gomock.Any(), gomock.Any()).
+					Return(volumeDTO, nil).
+					Times(1)
+			},
+		},
+		{
+			name:            "id not found",
+			isSetID:         false,
+			isSetAccountID:  true,
+			expectCode:      http.StatusBadRequest,
+			expectResponse:  map[string]any{"message": "invalid id"},
+			setMockVolumeUC: func(context.Context, *mockUsecase.MockVolumeUsecase) {},
+		},
+		{
+			name:            "account id not found",
+			isSetID:         true,
+			isSetAccountID:  false,
+			expectCode:      http.StatusInternalServerError,
+			expectResponse:  map[string]any{"message": "internal server error"},
+			setMockVolumeUC: func(context.Context, *mockUsecase.MockVolumeUsecase) {},
+		},
+		{
+			name:           "get error",
+			isSetID:        true,
+			isSetAccountID: true,
+			expectCode:     http.StatusInternalServerError,
+			expectResponse: map[string]any{"message": "internal server error"},
+			setMockVolumeUC: func(ctx context.Context, volumeUC *mockUsecase.MockVolumeUsecase) {
+				volumeUC.
+					EXPECT().
+					GetOne(ctx, gomock.Any(), gomock.Any()).
+					Return(nil, sql.ErrConnDone).
+					Times(1)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := t.Context()
+			w := httptest.NewRecorder()
+
+			c, _ := gin.CreateTestContext(w)
+			var err error
+			c.Request, err = http.NewRequestWithContext(ctx, "GET", "/volumes/"+id.String(), http.NoBody)
+			if err != nil {
+				t.Error(err)
+			}
+			if tt.isSetID {
+				c.Params = append(c.Params, gin.Param{Key: "id", Value: id.String()})
+			}
+			if tt.isSetAccountID {
+				c.Set("accountID", accountID)
+			}
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			volumeUC := mockUsecase.NewMockVolumeUsecase(ctrl)
+			tt.setMockVolumeUC(ctx, volumeUC)
+
+			hdl := handler.NewVolumeHandler(volumeUC)
+			hdl.GetOne(c)
+
+			c.Writer.WriteHeaderNow()
+
+			if w.Code != tt.expectCode {
+				t.Errorf("\nexpect: %v\ngot: %v", tt.expectCode, w.Code)
+			}
+
+			var response map[string]any
+			if err := json.Unmarshal(w.Body.Bytes(), &response); err != nil {
+				t.Error(err)
+			}
+			if diff := cmp.Diff(response, tt.expectResponse); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
