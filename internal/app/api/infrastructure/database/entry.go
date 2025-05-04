606ed6c49d54e594205a9b8fa40ab7c6a734f5ab
atsumarukun <atys.smz@gmail.com>
2025-05-05 00:15:52 +0900

    create: Entryリポジトリを実装.

--- internal/app/api/infrastructure/database/entry.go
+++ internal/app/api/infrastructure/database/entry.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 
 	"github.com/google/uuid"
@@ -9,6 +10,9 @@ import (
 
 	"github.com/atsumarukun/holos-storage-api/internal/app/api/domain/entity"
 	"github.com/atsumarukun/holos-storage-api/internal/app/api/domain/repository"
+	"github.com/atsumarukun/holos-storage-api/internal/app/api/infrastructure/database/model"
+	"github.com/atsumarukun/holos-storage-api/internal/app/api/infrastructure/database/pkg/transaction"
+	"github.com/atsumarukun/holos-storage-api/internal/app/api/infrastructure/database/transformer"
 	"github.com/atsumarukun/holos-storage-api/internal/app/api/pkg/status"
 	"github.com/atsumarukun/holos-storage-api/internal/app/api/pkg/status/code"
 )
@@ -26,9 +30,24 @@ func NewEntryRepository(db *sqlx.DB) repository.EntryRepository {
 }
 
 func (r *entryRepository) Create(ctx context.Context, entry *entity.Entry) error {
-	return errors.New("not implemented")
+	if entry == nil {
+		return ErrRequiredEntry
+	}
+
+	driver := transaction.GetDriver(ctx, r.db)
+	model := transformer.ToEntryModel(entry)
+	_, err := driver.NamedExecContext(ctx, "INSERT INTO entries (id, account_id, volume_id, key, size, type, is_public, created_at, updated_at) VALUES (:id, :account_id, :volume_id, :key, :size, :type, :is_public, :created_at, :updated_at);", model)
+	return err
 }
 
 func (r *entryRepository) FindOneByKeyAndVolumeIDAndAccountID(ctx context.Context, key string, volumeID uuid.UUID, accountID uuid.UUID) (*entity.Entry, error) {
-	return nil, errors.New("not implemented")
+	driver := transaction.GetDriver(ctx, r.db)
+	var model model.EntryModel
+	if err := driver.QueryRowxContext(ctx, `SELECT id, account_id, volume_id, key, size, type, is_public, created_at, updated_at FROM entries WHERE key = ? AND volume_id = ? AND account_id = ? LIMIT 1;`, key, volumeID, accountID).StructScan(&model); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return transformer.ToEntryEntity(&model), nil
 }
