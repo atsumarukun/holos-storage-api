797126af3b87bbb2cb8f4b4cba7cfd4ab553a24d
atsumarukun <atys.smz@gmail.com>
2025-05-12 01:02:44 +0900

    create: Entryサービス内Del;ete関数を実装.

--- internal/app/api/domain/service/entry.go
+++ internal/app/api/domain/service/entry.go
@@ -110,7 +110,31 @@ func (s *entryService) Update(ctx context.Context, volume *entity.Volume, entry 
 }
 
 func (s *entryService) Delete(ctx context.Context, volume *entity.Volume, entry *entity.Entry) error {
-	return errors.New("not implemented")
+	if volume == nil {
+		return ErrRequiredVolume
+	}
+	if entry == nil {
+		return ErrRequiredEntry
+	}
+
+	if entry.IsFolder() {
+		children, err := s.entryRepo.FindByKeyPrefixAndAccountID(ctx, entry.Key+"/", entry.AccountID)
+		if err != nil {
+			return err
+		}
+
+		for _, child := range children {
+			if err := s.entryRepo.Delete(ctx, child); err != nil {
+				return err
+			}
+		}
+	}
+
+	if err := s.entryRepo.Delete(ctx, entry); err != nil {
+		return err
+	}
+
+	return s.bodyRepo.Delete(volume.Name + "/" + entry.Key)
 }
 
 func (s *entryService) extractDirs(key string) []string {
