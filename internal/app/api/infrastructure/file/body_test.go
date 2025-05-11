edc7060f4f0054365ec95fb67ed4b365c8314f23
atsumarukun <atys.smz@gmail.com>
2025-05-11 17:49:27 +0900

    create: Bodyリポジトリ内Update関数のユニットテストコードを作成.

--- internal/app/api/infrastructure/file/body_test.go
+++ internal/app/api/infrastructure/file/body_test.go
@@ -43,8 +43,51 @@ func TestBody_Create(t *testing.T) {
 			if err != nil {
 				t.Error(err)
 			}
-
 			if exists != tt.expectResult {
+				t.Errorf("\nexpect: %v\ngot: %v", tt.expectResult, exists)
+			}
+		})
+	}
+}
+
+func TestBody_Update(t *testing.T) {
+	tests := []struct {
+		name         string
+		inputSrc     string
+		inputDst     string
+		expectResult bool
+		expectError  error
+	}{
+		{name: "success", inputSrc: "sample/test.txt", inputDst: "update/test.txt", expectResult: true, expectError: nil},
+		{name: "update error", inputSrc: "sample/test.txt", inputDst: "update/test.txt", expectResult: false, expectError: io.ErrNoProgress},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fs := afero.NewMemMapFs()
+			basePath := ""
+
+			if err := afero.WriteFile(fs, tt.inputSrc, []byte("test"), 0o755); err != nil {
+				t.Error(err)
+			}
+
+			repo := file.NewBodyRepository(fs, basePath)
+			if err := repo.Update(tt.inputSrc, tt.inputDst); !errors.Is(err, tt.expectError) {
+				t.Errorf("\nexpect: %v\ngot: %v", tt.expectError, err)
+			}
+
+			exists, err := afero.Exists(fs, basePath+tt.inputDst)
+			if err != nil {
+				t.Error(err)
+			}
+			if exists != tt.expectResult {
+				t.Errorf("\nexpect: %v\ngot: %v", tt.expectResult, exists)
+			}
+
+			exists, err = afero.Exists(fs, basePath+tt.inputSrc)
+			if err != nil {
+				t.Error(err)
+			}
+			if exists != !tt.expectResult {
 				t.Errorf("\nexpect: %v\ngot: %v", tt.expectResult, exists)
 			}
 		})
