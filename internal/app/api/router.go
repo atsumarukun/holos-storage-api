f5688e25002d3a376ab20f0f6434353604ea91dd
atsumarukun <atys.smz@gmail.com>
2025-05-29 22:35:39 +0900

    refactor: パスがスラッシュで終わらないように変更.

--- internal/app/api/router.go
+++ internal/app/api/router.go
@@ -4,19 +4,19 @@ import "github.com/gin-gonic/gin"
 
 func registerRouter(r *gin.Engine) {
 	health := r.Group("health")
-	health.GET("/", healthHdl.Health)
+	health.GET("", healthHdl.Health)
 
 	r.Use(authorizationMW.Authorize)
 
 	volumes := r.Group("volumes")
-	volumes.POST("/", volumeHdl.Create)
-	volumes.GET("/", volumeHdl.GetAll)
+	volumes.POST("", volumeHdl.Create)
+	volumes.GET("", volumeHdl.GetAll)
 	volumes.PUT("/:name", volumeHdl.Update)
 	volumes.DELETE("/:name", volumeHdl.Delete)
 	volumes.GET("/:name", volumeHdl.GetOne)
 
 	entries := r.Group("entries")
-	entries.POST("/", entryHdl.Create)
+	entries.POST("", entryHdl.Create)
 	entries.GET("/:volumeName", entryHdl.Search)
 	entries.PUT("/:volumeName/*key", entryHdl.Update)
 	entries.DELETE("/:volumeName/*key", entryHdl.Delete)
