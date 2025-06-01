e86426e7b83d01177147eb36298042ecf11083f8
atsumarukun <atys.smz@gmail.com>
2025-06-01 17:55:56 +0900

    create: Entryサービス内Copy関数のユニットテストコードを作成.

--- internal/app/api/domain/service/entry_test.go
+++ internal/app/api/domain/service/entry_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/google/uuid"
 	"go.uber.org/mock/gomock"
 
@@ -442,6 +444,159 @@ func TestEntry_DeleteDescendants(t *testing.T) {
 			serv := service.NewEntryService(entryRepo)
 			if err := serv.DeleteDescendants(ctx, tt.inputEntry); !errors.Is(err, tt.expectError) {
 				t.Errorf("\nexpect: %v\ngot: %v", tt.expectError, err)
+			}
+		})
+	}
+}
+
+func TestEntry_Copy(t *testing.T) {
+	accountID := uuid.New()
+	volumeID := uuid.New()
+	fileEntry := &entity.Entry{
+		ID:        uuid.New(),
+		AccountID: accountID,
+		VolumeID:  volumeID,
+		Key:       "key/sample.txt",
+		Size:      4,
+		Type:      "text/plain; charset=utf-8",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	folderEntry := &entity.Entry{
+		ID:        uuid.New(),
+		AccountID: accountID,
+		VolumeID:  volumeID,
+		Key:       "key",
+		Size:      0,
+		Type:      "folder",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	copiedFileEntry := &entity.Entry{
+		ID:        uuid.New(),
+		AccountID: fileEntry.AccountID,
+		VolumeID:  fileEntry.VolumeID,
+		Key:       "key/sample copy.txt",
+		Size:      fileEntry.Size,
+		Type:      fileEntry.Type,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	copiedFolderEntry := &entity.Entry{
+		ID:        uuid.New(),
+		AccountID: folderEntry.AccountID,
+		VolumeID:  folderEntry.VolumeID,
+		Key:       "key copy",
+		Size:      folderEntry.Size,
+		Type:      folderEntry.Type,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	copiedFileEntryTwice := &entity.Entry{
+		ID:        uuid.New(),
+		AccountID: fileEntry.AccountID,
+		VolumeID:  fileEntry.VolumeID,
+		Key:       "key/sample copy copy.txt",
+		Size:      fileEntry.Size,
+		Type:      fileEntry.Type,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	tests := []struct {
+		name             string
+		inputEntry       *entity.Entry
+		expectResult     *entity.Entry
+		expectError      error
+		setMockEntryRepo func(*mockRepository.MockEntryRepository)
+	}{
+		{
+			name:         "copy file entry",
+			inputEntry:   fileEntry,
+			expectResult: copiedFileEntry,
+			expectError:  nil,
+			setMockEntryRepo: func(entryRepo *mockRepository.MockEntryRepository) {
+				entryRepo.
+					EXPECT().
+					FindOneByKeyAndVolumeID(gomock.Any(), gomock.Any(), gomock.Any()).
+					Return(nil, repository.ErrEntryNotFound).
+					Times(1)
+			},
+		},
+		{
+			name:         "copy folder entry",
+			inputEntry:   folderEntry,
+			expectResult: copiedFolderEntry,
+			expectError:  nil,
+			setMockEntryRepo: func(entryRepo *mockRepository.MockEntryRepository) {
+				entryRepo.
+					EXPECT().
+					FindOneByKeyAndVolumeID(gomock.Any(), gomock.Any(), gomock.Any()).
+					Return(nil, repository.ErrEntryNotFound).
+					Times(1)
+			},
+		},
+		{
+			name:         "duplicate key",
+			inputEntry:   fileEntry,
+			expectResult: copiedFileEntryTwice,
+			expectError:  nil,
+			setMockEntryRepo: func(entryRepo *mockRepository.MockEntryRepository) {
+				entryRepo.
+					EXPECT().
+					FindOneByKeyAndVolumeID(gomock.Any(), gomock.Any(), gomock.Any()).
+					Return(copiedFileEntry, nil).
+					Times(1)
+				entryRepo.
+					EXPECT().
+					FindOneByKeyAndVolumeID(gomock.Any(), gomock.Any(), gomock.Any()).
+					Return(nil, repository.ErrEntryNotFound).
+					Times(1)
+			},
+		},
+		{
+			name:             "entry is nil",
+			inputEntry:       nil,
+			expectResult:     nil,
+			expectError:      service.ErrRequiredEntry,
+			setMockEntryRepo: func(*mockRepository.MockEntryRepository) {},
+		},
+		{
+			name:         "find entry error",
+			inputEntry:   fileEntry,
+			expectResult: nil,
+			expectError:  sql.ErrConnDone,
+			setMockEntryRepo: func(entryRepo *mockRepository.MockEntryRepository) {
+				entryRepo.
+					EXPECT().
+					FindOneByKeyAndVolumeID(gomock.Any(), gomock.Any(), gomock.Any()).
+					Return(nil, sql.ErrConnDone).
+					Times(1)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			ctx := t.Context()
+
+			entryRepo := mockRepository.NewMockEntryRepository(ctrl)
+			tt.setMockEntryRepo(entryRepo)
+
+			serv := service.NewEntryService(entryRepo)
+
+			result, err := serv.Copy(ctx, tt.inputEntry)
+			if !errors.Is(err, tt.expectError) {
+				t.Errorf("\nexpect: %v\ngot: %v", tt.expectError, err)
+			}
+
+			opts := cmp.Options{
+				cmpopts.IgnoreFields(entity.Entry{}, "ID", "CreatedAt", "UpdatedAt"),
+			}
+			if diff := cmp.Diff(tt.expectResult, result, opts...); diff != "" {
+				t.Error(diff)
 			}
 		})
 	}
