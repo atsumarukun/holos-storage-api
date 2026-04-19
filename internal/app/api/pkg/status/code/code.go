3c92d6f3b5f0b1b9bca9d1e3bd04fb42af30194b
atsumarukun <atys.smz@gmail.com>
2026-04-19 18:10:02 +0900

    update: ドメインルール違反時のステータスコードをUnprocessableContentに変更.

--- internal/app/api/pkg/status/code/code.go
+++ internal/app/api/pkg/status/code/code.go
@@ -3,10 +3,11 @@ package code
 type StatusCode string
 
 var (
-	BadRequest   StatusCode = "BAD_REQUEST"
-	Unauthorized StatusCode = "UNAUTHORIZED"
-	Forbidden    StatusCode = "FORBIDDEN"
-	NotFound     StatusCode = "NOT_FOUND"
-	Conflict     StatusCode = "CONFLICT"
-	Internal     StatusCode = "INTERNAL"
+	BadRequest           StatusCode = "BAD_REQUEST"
+	Unauthorized         StatusCode = "UNAUTHORIZED"
+	Forbidden            StatusCode = "FORBIDDEN"
+	NotFound             StatusCode = "NOT_FOUND"
+	Conflict             StatusCode = "CONFLICT"
+	UnprocessableContent StatusCode = "UNPROCESSABLE_CONTENT"
+	Internal             StatusCode = "INTERNAL"
 )
